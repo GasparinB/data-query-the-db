113a07f5d806d960231875e5d668f5fcd625bf5d
gaspar burgi <gasparburgi49@gmail.com>
2023-08-26 11:59:57 -0300

    2nd

--- queries.py
+++ queries.py
@@ -2,15 +2,36 @@
 
 def query_orders(db):
     # return a list of orders displaying each column
-    pass  # YOUR CODE HERE
+    query="""
+    SELECT *
+    FROM Orders o
+    ORDER BY o.OrderID ASC
+    """
+    db.execute(query)
+    c=db.fetchall()
+    return c
 
 def get_orders_range(db, date_from, date_to):
     # return a list of orders displaying all columns with OrderDate between
     # date_from and date_to (excluding date_from and including date_to)
-    pass  # YOUR CODE HERE
-
+    query="""
+    SELECT *
+    FROM Orders o
+    WHERE o.OrderDate >?
+    AND o.OrderDate  < ?
+    """
+    db.execute(query,(date_from,date_to))
+    c=db.fetchall()
+    return c
 def get_waiting_time(db):
     # get a list with all the orders displaying each column
     # and calculate an extra TimeDelta column displaying the number of days
     # between OrderDate and ShippedDate, ordered by ascending TimeDelta
-    pass  # YOUR CODE HERE
+    query="""
+    SELECT *, JULIANDAY(o.ShippedDate)-JULIANDAY(o.OrderDate) as TimeDelta
+    FROM Orders o
+    ORDER BY TimeDelta
+    """
+    db.execute(query)
+    c=db.fetchall()
+    return c
